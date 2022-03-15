b67bda2965dae91016977accbdbad2fd6e63eb02
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-15 16:00:55 +0100

    ATLAS-549: print log

--- frontend/npm_publish.sh
+++ frontend/npm_publish.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Prepared version..."
+echo "Prepare atlas-frontend version..."
 echo ${VERSION}
+npm version ${VERSION}
+echo "atlas-frontend-${VERSION} successfully prepared"
 
-echo "npm publish atlas frontend"
+echo "npm publish atlas-frontend-${VERSION}"
 touch .npmrc
 echo "registry=https://bin.sbb.ch/artifactory/api/npm/atlas.npm/" >> .npmrc
 echo ${NPM_AUTH}
@@ -11,5 +13,9 @@ echo _auth = ${NPM_AUTH} >> .npmrc
 echo  email = antonio.romano@sbb.ch >> .npmrc
 echo always-auth = true >> .npmrc
 
-npm publish --userconfig=.npmrc
+echo "Read .npmrc"
+cat .npmrc
 
+npm publish --userconfig=.npmrc --loglevel verbose
+
+echo "atlas-frontend-${VERSION} successfully published"
