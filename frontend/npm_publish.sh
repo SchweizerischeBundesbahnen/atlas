51753f504673074233a0e1a9afc2f9b021fd78bf
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-15 11:55:14 +0100

    ATLAS-549: try to publish

--- frontend/npm_publish.sh
+++ frontend/npm_publish.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 echo "npm publish"
-npm publish --registry https://bin.sbb.ch/artifactory/api/npm/atlas.npm/
+touch ~/.npmrc
+curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PWD} https://bin.sbb.ch/artifactory/api/npm/auth >> ~/.npmrc
+
+npm publish
