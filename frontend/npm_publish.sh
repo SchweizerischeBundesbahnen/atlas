fb5f2e89b5eea17e86a8bf19788c5c1adb1a99f0
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-15 12:13:13 +0100

    ATLAS-549: create .npmrc on root project

--- frontend/npm_publish.sh
+++ frontend/npm_publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "npm publish"
-touch ~/.npmrc
-curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PWD} https://bin.sbb.ch/artifactory/api/npm/auth >> ~/.npmrc
+touch .npmrc
+curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PWD} https://bin.sbb.ch/artifactory/api/npm/auth >> .npmrc
 
 npm publish
