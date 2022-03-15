bf579088b0ccb55781dff7b42f192e52f6ce1509
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-15 17:04:28 +0100

    ATLAS-549: try with no registry specification

--- frontend/npm_publish.sh
+++ frontend/npm_publish.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Prepare atlas-frontend version..."
-echo ${VERSION}
+#echo "Prepare atlas-frontend version..."
+#echo ${VERSION}
 #npm version ${VERSION}
 #echo "atlas-frontend-${VERSION} successfully prepared"
 
-#echo "npm publish atlas-frontend-${VERSION}"
+echo "npm publish atlas-frontend-${VERSION}"
 touch .npmrc
 #echo "registry=https://bin.sbb.ch/artifactory/api/npm/atlas.npm/" >> .npmrc
+#echo "registry=https://bin.sbb.ch/artifactory/api/npm/npm/" >> .npmrc
 echo ${NPM_AUTH}
 echo _auth = ${NPM_AUTH} >> .npmrc
 echo  email = antonio.romano@sbb.ch >> .npmrc
@@ -16,6 +17,6 @@ echo always-auth = true >> .npmrc
 echo "Read .npmrc"
 cat .npmrc
 
-npm publish --userconfig=.npmrc --loglevel verbose
+npm publish --userconfig=.npmrc --registry=https://bin.sbb.ch/artifactory/api/npm/atlas.npm/ --loglevel verbose
 
 echo "atlas-frontend-${VERSION} successfully published"
