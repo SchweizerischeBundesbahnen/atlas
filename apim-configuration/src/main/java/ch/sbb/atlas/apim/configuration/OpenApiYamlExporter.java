56d3e289db00bec2e9b7468f2e2d38602b4462e3
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-10 15:59:00 +0100

    ATLAS-894: frontend code clean up

--- apim-configuration/src/main/java/ch/sbb/atlas/apim/configuration/OpenApiYamlExporter.java
+++ apim-configuration/src/main/java/ch/sbb/atlas/apim/configuration/OpenApiYamlExporter.java
@@ -2,16 +2,15 @@ package ch.sbb.atlas.apim.configuration;
 
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
-import lombok.experimental.UtilityClass;
-
 import java.io.IOException;
 import java.nio.file.Paths;
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class OpenApiYamlExporter {
 
   public static void export(OpenAPI openAPI, String targetDir) {
-      writeToFile(targetDir, openAPI);
+    writeToFile(targetDir, openAPI);
   }
 
   private static void writeToFile(String dir, OpenAPI openAPI) {
