5b85e83777c6e5a5768a0d009497dbc5c4586b16
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-20 11:34:53 +0100

    ATLAS-1541: add 2. and 3. stopPoint use cases

--- frontend/cypress/e2e/sepodi/1-add-swiss-stop-point.cy.ts
+++ frontend/cypress/e2e/sepodi/1-add-swiss-stop-point.cy.ts
@@ -3,7 +3,7 @@ import CommonUtils from "../../support/util/common-utils";
 import {DataCy} from "../../support/data-cy";
 import BodiDependentUtils from "../../support/util/bodi-dependent-utils";
 
-describe('Use case: Add a swiss stopPoint ', {testIsolation: false}, () => {
+describe('SePoDi use cases', {testIsolation: false}, () => {
 
   const servicePoint = SepodiUtils.getServicePointVersion();
 
@@ -15,79 +15,97 @@ describe('Use case: Add a swiss stopPoint ', {testIsolation: false}, () => {
     BodiDependentUtils.createDependentBusinessOrganisation();
   });
 
+  describe('Use case 1: Add a swiss stopPoint ', () => {
 
-  it('Step-2: Navigate to Dienststellen', () => {
-    SepodiUtils.navigateToServicePoint();
+    it('Step-2: Navigate to Dienststellen', () => {
+      SepodiUtils.navigateToServicePoint();
+    });
+
+    it('Step-3: Click on Neue Dienststelle', () => {
+      cy.get(DataCy.SEPODI_NEW_SERVICE_POINT_BUTTON).click();
+      cy.get(DataCy.SEPODI_NEW_SERVICE_POINT_LABEL).should('contain.text', 'Neue Dienststelle');
+    });
+
+    it('Step-4: Fill form', () => {
+      CommonUtils.selectItemFromDropDown(DataCy.SEPODI_SELECT_COUNTRY, '85 - Schweiz');
+      CommonUtils.getClearType(DataCy.SEPODI_DESIGNATION_OFFICIAL, servicePoint.designationOfficial);
+      CommonUtils.getClearType(DataCy.SEPODI_DESIGNATION_LONG, servicePoint.designationLong);
+      CommonUtils.getClearType(DataCy.VALID_FROM, servicePoint.validFrom, true);
+      CommonUtils.getClearType(DataCy.VALID_TO, servicePoint.validTo, true);
+      CommonUtils.typeAndSelectItemFromDropDown(
+        DataCy.BUSINESS_ORGANISATION + ' ' + 'input',
+        servicePoint.businessOrganisation
+      );
+      cy.get('[type="radio"]').check('STOP_POINT');
+      cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).check();
+      cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).should('be.checked');
+      cy.get(DataCy.MEANS_OF_TRANSPORT_TRAIN).click();
+      CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_EAST, servicePoint.east);
+      CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_NORTH, servicePoint.north);
+      CommonUtils.selectItemFromDropDown(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION, 'Ordentliche Haltestelle');
+      cy.get(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION).type('{esc}')
+      CommonUtils.selectItemFromDropDown(DataCy.SEPODI_CATEGORIES, 'Billettautomat SBB');
+      cy.get(DataCy.SEPODI_CATEGORIES).type('{esc}')
+      SepodiUtils.saveServicePoint();
+    });
+
+    it('Step-5: Search added StopPoint', () => {
+      CommonUtils.navigateToHomeViaHomeLogo();
+      SepodiUtils.navigateToServicePoint();
+      cy.get(DataCy.SEPODI_SEARCH_SERVICE_POINT_SELECT + ' input')
+        .type(servicePoint.designationOfficial)
+        .then(() => {
+          cy.intercept('POST', 'service-point-directory/v1/service-points/search').as('searchVersion');
+          cy.wait('@searchVersion').its('response.statusCode').should('eq', 200);
+          cy.get(DataCy.SEPODI_SEARCH_SERVICE_POINT_SELECT + ' .ng-option').click();
+        })
+    });
+
+    it('Step-6: Validate added StopPoint', () => {
+      CommonUtils.getTotalRange().should('contain', servicePoint.validFrom).should('contain', servicePoint.validTo);
+      CommonUtils.assertVersionRange(1, servicePoint.validFrom, servicePoint.validTo);
+      CommonUtils.assertVersionStatus(1, 'Entwurf');
+      CommonUtils.assertItemValue(DataCy.SEPODI_DESIGNATION_OFFICIAL, servicePoint.designationOfficial);
+      CommonUtils.assertItemValue(DataCy.SEPODI_DESIGNATION_LONG, servicePoint.designationLong);
+      cy.get(DataCy.BUSINESS_ORGANISATION).should('contain.text', servicePoint.businessOrganisation);
+      CommonUtils.assertItemValue(DataCy.VALID_FROM, servicePoint.validFrom);
+      CommonUtils.assertItemValue(DataCy.VALID_TO, servicePoint.validTo);
+      cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).should('be.checked');
+      cy.get(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION + ' span').should('contain.text', 'Ordentliche Haltestelle')
+      CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_EAST, servicePoint.east);
+      CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_NORTH, servicePoint.north);
+      CommonUtils.assertItemsFromDropdownAreChecked(DataCy.SEPODI_CATEGORIES, ['Billettautomat SBB'])
+    });
+
   });
 
-  it('Step-3: Click on Neue Dienststelle', () => {
-    cy.get(DataCy.SEPODI_NEW_SERVICE_POINT_BUTTON).click();
-    cy.get(DataCy.SEPODI_NEW_SERVICE_POINT_LABEL).should('contain.text', 'Neue Dienststelle');
+  describe('Use case 2: change geografie', () => {
+
+    it('Step-1: update geografie', () => {
+      cy.get(DataCy.EDIT).click();
+      CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_EAST, '2600783');
+      CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_NORTH, '1201099');
+      CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_HEIGHT, '555');
+      SepodiUtils.saveServicePoint();
+      CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_EAST, '2600783');
+      CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_NORTH, '1201099');
+      CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_HEIGHT, '555');
+    });
+
   });
 
-  it('Step-4: Fill form', () => {
-    CommonUtils.selectItemFromDropDown(DataCy.SEPODI_SELECT_COUNTRY, '85 - Schweiz');
-    CommonUtils.getClearType(DataCy.SEPODI_DESIGNATION_OFFICIAL, servicePoint.designationOfficial);
-    CommonUtils.getClearType(DataCy.SEPODI_DESIGNATION_LONG, servicePoint.designationLong);
-    CommonUtils.getClearType(DataCy.VALID_FROM, servicePoint.validFrom, true);
-    CommonUtils.getClearType(DataCy.VALID_TO, servicePoint.validTo, true);
-    CommonUtils.typeAndSelectItemFromDropDown(
-      DataCy.BUSINESS_ORGANISATION + ' ' + 'input',
-      servicePoint.businessOrganisation
-    );
-    cy.get('[type="radio"]').check('STOP_POINT');
-    cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).check();
-    cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).should('be.checked');
-    cy.get(DataCy.MEANS_OF_TRANSPORT_TRAIN).click();
-    CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_EAST, servicePoint.east);
-    CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_NORTH, servicePoint.north);
-    CommonUtils.getClearType(DataCy.SEPODI_GEOLOCATION_HEIGHT, servicePoint.height);
-    CommonUtils.selectItemFromDropDown(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION, 'Ordentliche Haltestelle');
-    cy.get(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION).type('{esc}')
-    CommonUtils.selectItemFromDropDown(DataCy.SEPODI_CATEGORIES, 'Billettautomat SBB');
-    cy.get(DataCy.SEPODI_CATEGORIES).type('{esc}')
-    cy.get(DataCy.SAVE_ITEM).click().then(() =>
-    {
-      cy.intercept('GET', 'service-point-directory/v1/service-points/85*').as('saveAndGetVersion');
-      cy.wait('@saveAndGetVersion').its('response.statusCode').should('eq', 200);
-      cy.get(DataCy.EDIT).should('exist');
-      cy.get(DataCy.CLOSE_DETAIL).should('exist');
-      cy.get(DataCy.REVOKE_ITEM).should('exist');
-      cy.get(DataCy.SKIP_WORKFLOW).should('exist');
-    })
-  });
+  describe('Use case 3: StopPoint cancellation/termination', () => {
 
-  it('Step-5: Search added StopPoint', () => {
-    CommonUtils.navigateToHomeViaHomeLogo();
-    SepodiUtils.navigateToServicePoint();
-    cy.get(DataCy.SEPODI_SEARCH_SERVICE_POINT_SELECT + ' input')
-      .type(servicePoint.designationOfficial)
-      .then(() =>{
-        cy.intercept('POST', 'service-point-directory/v1/service-points/search').as('searchVersion');
-        cy.wait('@searchVersion').its('response.statusCode').should('eq', 200);
-        cy.get(DataCy.SEPODI_SEARCH_SERVICE_POINT_SELECT + ' .ng-option').click();
-      })
-  });
+    it('Step-1: update validTo', () => {
+      cy.get(DataCy.EDIT).click();
+      const terminationDate = '31.12.2098';
+      CommonUtils.getClearType(DataCy.VALID_TO, terminationDate, true);
+      SepodiUtils.saveServicePoint();
+      CommonUtils.assertItemValue(DataCy.VALID_TO, terminationDate);
+      CommonUtils.assertVersionRange(1, servicePoint.validFrom, terminationDate);
+      CommonUtils.getTotalRange().should('contain', servicePoint.validFrom).should('contain', terminationDate);
+    });
 
-  it('Step-6: Validate added StopPoint', () => {
-    CommonUtils.getTotalRange().should('contain', servicePoint.validFrom).should('contain', servicePoint.validTo);
-    CommonUtils.assertVersionRange(1, servicePoint.validFrom, servicePoint.validTo);
-    CommonUtils.assertVersionStatus(1,'Entwurf');
-    CommonUtils.assertItemValue(DataCy.SEPODI_DESIGNATION_OFFICIAL, servicePoint.designationOfficial);
-    CommonUtils.assertItemValue(DataCy.SEPODI_DESIGNATION_LONG, servicePoint.designationLong);
-    cy.get(DataCy.BUSINESS_ORGANISATION).should('contain.text', servicePoint.businessOrganisation);
-    CommonUtils.assertItemValue(DataCy.VALID_FROM, servicePoint.validFrom);
-    CommonUtils.assertItemValue(DataCy.VALID_TO, servicePoint.validTo);
-    cy.get(DataCy.SEPODI_CHECKBOX_STOP_POINT).should('be.checked');
-    cy.get(DataCy.SEPODI_STOP_POINT_TYPE_SELECTION + ' span').should('contain.text','Ordentliche Haltestelle')
-    CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_EAST, servicePoint.east);
-    CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_NORTH, servicePoint.north);
-    // CommonUtils.assertItemValue(DataCy.SEPODI_GEOLOCATION_HEIGHT, servicePoint.height);
-    CommonUtils.assertItemsFromDropdownAreChecked(DataCy.SEPODI_CATEGORIES, ['Billettautomat SBB'])
-  });
-
-  it('Dependent BusinessOrganisation Cleanup Step', () => {
-    BodiDependentUtils.deleteDependentBusinessOrganisation();
   });
 
 });
--- frontend/cypress/support/util/sepodi-utils.ts
+++ frontend/cypress/support/util/sepodi-utils.ts
@@ -1,11 +1,25 @@
 import CommonUtils from "./common-utils";
 import BodiDependentUtils from "./bodi-dependent-utils";
+import {DataCy} from "../data-cy";
 
 export default class SepodiUtils {
   static navigateToServicePoint() {
     CommonUtils.navigateToHomeViaHomeLogo();
     cy.get('#service-point-directory').click();
   }
+
+  static saveServicePoint(){
+    cy.get(DataCy.SAVE_ITEM).click().then(() =>
+    {
+      cy.intercept('GET', 'service-point-directory/v1/service-points/85*').as('saveAndGetVersion');
+      cy.wait('@saveAndGetVersion').its('response.statusCode').should('eq', 200);
+      cy.get(DataCy.EDIT).should('exist');
+      cy.get(DataCy.CLOSE_DETAIL).should('exist');
+      cy.get(DataCy.REVOKE_ITEM).should('exist');
+      cy.get(DataCy.SKIP_WORKFLOW).should('exist');
+    })
+  }
+
 
   static getServicePointVersion() {
     return {
@@ -15,7 +29,7 @@ export default class SepodiUtils {
       validTo: '31.12.9999',
       north: '1201099.1',
       east: '2600783.1',
-      height: '555.1'
+      height: '554.1'
     }
   }
 
