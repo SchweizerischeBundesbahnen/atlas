490726b0953b9fd48cd7af78673514bda63f11fb
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-26 16:08:08 +0100

    joho: Finish TTH-10-email-Release-Test

--- frontend/cypress/release/api/tth/MoreThan1EmailAddress/CreateStatement.ts
+++ frontend/cypress/release/api/tth/MoreThan1EmailAddress/CreateStatement.ts
@@ -1,89 +1,73 @@
-import {getXHRResponse} from "rxjs/internal/ajax/getXHRResponse";
+import TthUtils from "../../../../support/util/tth-utils";
+import CommonUtils from "../../../../support/util/common-utils";
 
 describe('TTH: Create a statement with several email-addresses', {testIsolation: false}, () => {
 
-  const ACTIVE_YEAR_MAX_COUNT = 1;
-  const status = Object.freeze({
-    ACTIVE: 'ACTIVE',
-    PLANNED: 'PLANNED',
-    ARCHIVED: 'ARCHIVED'
-  });
-  const allStatusChoices = [status.PLANNED, status.ACTIVE, status.ARCHIVED].join(',');
+  const allStatusChoices = [TthUtils.status.PLANNED, TthUtils.status.ACTIVE, TthUtils.status.ARCHIVED].join(',');
 
-  let nextUnoccupiedYear = 0;
+  let timetableYear = -1;
   let sboid = "";
-  let ttfnid = "";
+  let ttfnId = "";
+  let statementId = -1;
 
   const today = new Date();
   const tomorrow = new Date();
   tomorrow.setDate(today.getDate() + 1);
+  const currentYear = today.getFullYear();
 
-  const get = (url: string) => {
-    return cy.request({
-      method: 'GET',
-      url: Cypress.env('API_URL') + url,
-      headers: {
-        Authorization: `Bearer ${window.sessionStorage.getItem('access_token')}`
-      }
-    });
-  }
-
-  const post = (url: string, body: object = {}) => {
-    return cy.request({
-      method: 'POST',
-      url: Cypress.env('API_URL') + url,
-      body: body,
-      headers: {
-        Authorization: `Bearer ${window.sessionStorage.getItem('access_token')}`
-      }
-    });
-  }
-
-  const postStatement = (url: string, body: object) => {
-    const statement = new FormData();
-    statement.append("statement", new Blob([JSON.stringify(body)], { type: "application/json" }), "statement");
-
-     return cy.request({
-        method: "POST",
-        url: Cypress.env('API_URL') + url,
-        headers: {
-          Authorization: `Bearer ${window.sessionStorage.getItem('access_token')}`,
-          "Content-Type": "multipart/form-data",
-        },
-        body: statement,
-      })
-  }
 
   it('Step-1: Login on ATLAS', () => {
     cy.atlasLogin();
   });
 
   it('Step-2: Close active year, if available', () => {
-    const yearStatus = 'ACTIVE';
-    const currentYear = new Date().getFullYear();
-
-    get('/line-directory/v1/timetable-hearing/years?statusChoices=' + yearStatus).then((response) => {
+    CommonUtils.get('/line-directory/v1/timetable-hearing/years?statusChoices=' + TthUtils.status.ACTIVE)
+      .then((response) => {
       expect(response.status).to.eq(200);
       expect(response).property('body').to.be.an('array');
 
       // When: There is a timetable hearing year active...
       if (response.body.length > 0) {
-        expect(response.body.length).to.eq(ACTIVE_YEAR_MAX_COUNT);
-        expect(response.body[0].timetableYear).to.be.greaterThan(currentYear);
+        expect(response.body.length).to.eq(TthUtils.ACTIVE_YEAR_MAX_COUNT);
+        expect(response.body[0]).property('timetableYear').to.be.greaterThan(currentYear);
 
         // Then: ... close the active year
-        post('/line-directory/v1/timetable-hearing/years/' + response.body[0].timetableYear + '/close', {}).then((response) => {
+        CommonUtils.post('/line-directory/v1/timetable-hearing/years/' + response.body[0].timetableYear + '/close', {}).then((response) => {
           expect(response).property('status').to.equal(200);
         });
       }
     });
   });
 
-  it('Step-3: Create a timetable hearing year', () => {
-    const createTthYear = () => {
-      const hearingYear = nextUnoccupiedYear - 1;
-      post('/line-directory/v1/timetable-hearing/years', {
-        timetableYear: nextUnoccupiedYear,
+  it('Step-3: Determine the next free timetable hearing year', () => {
+    // Given: There is no active timetable hearing year
+    CommonUtils.get('/line-directory/v1/timetable-hearing/years?statusChoices=' + allStatusChoices)
+      .then((response) => {
+      expect(response).property('status').to.equal(200);
+      expect(response.body).to.be.an('array');
+
+      const tthYears = response.body
+      const activeYears = tthYears.filter(year => year.hearingStatus === TthUtils.status.ACTIVE).map(year => year.timetableYear);
+      expect(activeYears.length, "There shouldn't be any active year.").to.eq(0);
+
+      const usedYears = tthYears.filter(year =>
+        (year.hearingStatus === TthUtils.status.PLANNED ||
+          year.hearingStatus === TthUtils.status.ARCHIVED)).map(year => year.timetableYear);
+
+      // The TTH-years are always one year later than the current one.
+      timetableYear = currentYear + 1;
+
+      // When: There is another next unused year...
+      while (usedYears.includes(timetableYear)) {
+        timetableYear++; // Then: Increment the year until we find one that's not used
+      }
+    });
+  });
+
+  it('Step-4: Create a timetable hearing year', () => {
+      const hearingYear = timetableYear - 1;
+      CommonUtils.post('/line-directory/v1/timetable-hearing/years', {
+        timetableYear: timetableYear,
         hearingFrom: `${hearingYear}-05-01`,
         hearingTo: `${hearingYear}-05-31`,
         statementCreatableExternal: true,
@@ -91,52 +75,25 @@ describe('TTH: Create a statement with several email-addresses', {testIsolation:
         statementEditable: true
       }).then((response) => {
         expect(response).property('status').to.equal(201);
-        expect(response).property('body').property('hearingStatus').to.eq(status.PLANNED);
+        expect(response).property('body').property('hearingStatus').to.eq(TthUtils.status.PLANNED);
       });
-    }
-
-    // Given: There is no active timetable hearing year
-    get('/line-directory/v1/timetable-hearing/years?statusChoices=' + allStatusChoices).then((response) => {
-      expect(response).property('status').to.equal(200);
-      expect(response.body).to.be.an('array');
-
-      const tthYears = response.body
-      const activeYears = tthYears.filter(year => year.hearingStatus === status.ACTIVE).map(year => year.timetableYear);
-      expect(activeYears.length, "There shouldn't be any active year.").to.eq(0);
-
-      const usedYears = tthYears.filter(year =>
-        (year.hearingStatus === status.PLANNED ||
-          year.hearingStatus === status.ARCHIVED)).map(year => year.timetableYear);
-
-      // The TTH-years are always one year later than the current one.
-      nextUnoccupiedYear = new Date().getFullYear() + 1;
-
-      // When: There is another next unused year...
-      while (usedYears.includes(nextUnoccupiedYear)) {
-        nextUnoccupiedYear++; // Increment the year until we find one that's not used
-      }
-      console.log(nextUnoccupiedYear)
-      // Then: Create the next year
-      createTthYear();
-    });
   });
 
 
-  it('Step-4: Start the timetable hearing year', () => {
-    post(`/line-directory/v1/timetable-hearing/years/${nextUnoccupiedYear}/start`).then((response) => {
+  it('Step-5: Start the timetable hearing year', () => {
+    CommonUtils.post(`/line-directory/v1/timetable-hearing/years/${timetableYear}/start`).then((response) => {
       expect(response).property('status').to.equal(200);
-      expect(response).property('body').property('hearingStatus').to.eq(status.ACTIVE);
-      console.log(nextUnoccupiedYear)
+      expect(response).property('body').property('hearingStatus').to.eq(TthUtils.status.ACTIVE);
     });
   });
 
-  it('Step-5: Create dependent Business Organisation', () => {
+  it('Step-6: Create dependent Business Organisation', () => {
     const generateRandomString = (length) => {
       const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
       return Array.from({ length }, () => characters.charAt(Math.floor(Math.random() * characters.length))).join('');
     };
 
-    post("/business-organisation-directory/v1/business-organisations/versions", {
+    CommonUtils.post("/business-organisation-directory/v1/business-organisations/versions", {
       descriptionDe: generateRandomString(10),
       descriptionFr: generateRandomString(10),
       descriptionIt: generateRandomString(10),
@@ -151,13 +108,13 @@ describe('TTH: Create a statement with several email-addresses', {testIsolation:
     }).then((response) => {
       expect(response).property('status').to.equal(201);
 
-      expect(response).property('body').property('sboid').to.exist;
+      expect(response).property('body').property('sboid').to.exist.and.be.a('string');
       sboid = response.body.sboid;
     })
   });
 
-  it('Step-5: Create dependent Time Table Field Number', () => {
-    post("/line-directory/v1/field-numbers/versions", {
+  it('Step-7: Create dependent Time Table Field Number', () => {
+    CommonUtils.post("/line-directory/v1/field-numbers/versions", {
       swissTimetableFieldNumber: Cypress._.random(10000, 99999).toString(),
       validFrom: today.toISOString().split('T')[0],
       validTo: tomorrow.toISOString().split('T')[0],
@@ -165,15 +122,14 @@ describe('TTH: Create a statement with several email-addresses', {testIsolation:
       number: Cypress._.random(10000, 99999).toString(),
     }).then((response) => {
       expect(response).property('status').to.equal(201);
-      expect(response).property('body').property('ttfnid').to.exist;
-      ttfnid = response.body.ttfnid;
+      expect(response).property('body').property('ttfnid').to.exist.and.be.a('string');
+      ttfnId = response.body.ttfnid;
     })
   });
 
-  it('Step-7: Create statement in active timetable hearing year', () => {
-    postStatement("/line-directory/v2/timetable-hearing/statements", {
-      statementStatus: "JUNK",
-      ttfnid: ttfnid,
+  it('Step-8: Create statement with 10 email-addresses in active timetable hearing year', () => {
+    TthUtils.postStatement("/line-directory/v2/timetable-hearing/statements", {
+      ttfnid: ttfnId,
       statementSender: {
         emails: [
           "tmp1@sbb.ch",
@@ -190,16 +146,59 @@ describe('TTH: Create a statement with several email-addresses', {testIsolation:
       },
       swissCanton: "BERN",
       statement: "I need some more busses please.",
-      timetableYear: nextUnoccupiedYear,
+      timetableYear: timetableYear,
     }).then((response) => {
       expect(response).property('status').to.equal(201);
     })
   });
 
-  it('Step-8: Close the timetable hearing year', () => {
-    post(`/line-directory/v1/timetable-hearing/years/${nextUnoccupiedYear}/close`).then((response) => {
+
+  it('Step-9: Get statement values', () => {
+    CommonUtils.get("/line-directory/v2/timetable-hearing/statements" +
+      `?timetableHearingYear=${timetableYear}
+      &statusRestrictions=RECEIVED
+      &ttfnid=${ttfnId}`)
+      .then((response) => {
+        expect(response).property('status').to.equal(200);
+        expect(response).property('body').property('objects').to.exist
+          .and.to.be.an('array').that.has.lengthOf(1);
+
+        const objects = response.body.objects[0]
+        expect(objects).property('statementSender').to.exist
+          .and.to.haveOwnProperty('emails').that.has.lengthOf(10);
+
+        expect(objects).property('id').to.exist
+        statementId = objects.id;
+
+        expect(objects).property('timetableYear').to.eq(timetableYear);
+      })
+  });
+
+  it('Step-10: Change statement status to PARTLY_IMPLEMENTED', () => {
+    CommonUtils.put("/line-directory/v2/timetable-hearing/statements/update-statement-status", {
+      "ids": [
+        statementId
+      ],
+      "statementStatus": "PARTLY_IMPLEMENTED",
+      "justification": "We are not done yet."
+    }).then((response) => {
       expect(response).property('status').to.equal(200);
-      expect(response).property('body').property('hearingStatus').to.eq(status.ARCHIVED);
+    })
+  });
+
+  it('Step-11: Close the timetable hearing year', () => {
+    CommonUtils.post(`/line-directory/v1/timetable-hearing/years/${timetableYear}/close`).then((response) => {
+      expect(response).property('status').to.equal(200);
+      expect(response).property('body').property('hearingStatus').to.eq(TthUtils.status.ARCHIVED);
+      expect(response).property('body').property('timetableYear').to.eq(timetableYear);
     });
+  });
+
+  it('Step-12: Check statement is in ARCHIVED timetable year', () => {
+    CommonUtils.get(`/line-directory/v2/timetable-hearing/statements/${statementId}`)
+      .then((response) => {
+        expect(response).property('status').to.equal(200);
+        expect(response).property('body').property('timetableYear').to.eq(timetableYear);
+      })
   });
 });
