457af75661798abfed6fd20bb78125bcbc78c5b8
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-11 10:20:02 +0100

    joho: Adjust LiDi-Release-Tests to new requirements (line.number/description = mandatory).

--- frontend/cypress/release/api/lidi/LineCRU.ts
+++ frontend/cypress/release/api/lidi/LineCRU.ts
@@ -1,10 +1,10 @@
 import CommonUtils from '../../../support/util/common-utils';
-import ReleaseApiUtils from "../../../support/util/release-api-utils";
+import ReleaseApiUtils from '../../../support/util/release-api-utils';
 
-const VALID_FROM = "2024-08-01";
+const VALID_FROM = '2024-08-01';
 describe('LiDi: Scenario Line-CRUD: New Line', { testIsolation: false }, () => {
-  let slnid = "";
-  let sboid = "";
+  let slnid = '';
+  let sboid = '';
   let lineVersionId = -1;
   let etagVersion = -1;
 
@@ -13,20 +13,25 @@ describe('LiDi: Scenario Line-CRUD: New Line', { testIsolation: false }, () => {
   });
 
   it('Step-2: Create dependent Business Organisation', () => {
-    CommonUtils.createDependentBusinessOrganisation(ReleaseApiUtils.today(), ReleaseApiUtils.today()).then((sboidOfBO:string) => {
-      sboid = sboidOfBO
+    CommonUtils.createDependentBusinessOrganisation(
+      ReleaseApiUtils.today(),
+      ReleaseApiUtils.today()
+    ).then((sboidOfBO: string) => {
+      sboid = sboidOfBO;
     });
   });
 
   it('Step-3: Create a new line version', () => {
     CommonUtils.post('/line-directory/v2/lines/versions', {
       swissLineNumber: ReleaseApiUtils.today(),
-      lineType: "ORDERLY",
+      lineType: 'ORDERLY',
       businessOrganisation: sboid,
       validFrom: VALID_FROM,
-      validTo: "2024-08-02",
-      lineConcessionType: "RACK_FREE_TRIPS",
-      offerCategory: "SL"
+      validTo: '2024-08-02',
+      lineConcessionType: 'RACK_FREE_TRIPS',
+      offerCategory: 'SL',
+      description: 'This is just a description.',
+      number: '1111111',
     }).then((response) => {
       expect(response.status).to.equal(201);
 
@@ -40,30 +45,46 @@ describe('LiDi: Scenario Line-CRUD: New Line', { testIsolation: false }, () => {
 
   it('Step-4: Read the line version', () => {
     // It is expected that slnid has already been set from a previous step
-    CommonUtils.get(`/line-directory/v2/lines/versions/${slnid}`).then((response) => {
-      const lineVersionsFirst = ReleaseApiUtils.makeCommonChecks(response, slnid, lineVersionId);
+    CommonUtils.get(`/line-directory/v2/lines/versions/${slnid}`).then(
+      (response) => {
+        const lineVersionsFirst = ReleaseApiUtils.makeCommonChecks(
+          response,
+          slnid,
+          lineVersionId
+        );
 
-      expect(lineVersionsFirst).to.have.property('swissLineNumber').that.is.a('string');
+        expect(lineVersionsFirst)
+          .to.have.property('swissLineNumber')
+          .that.is.a('string');
 
-      // Store the ETag version in a variable
-      etagVersion = lineVersionsFirst.etagVersion;
-      expect(etagVersion).to.exist.and.be.a('number');
-    });
+        // Store the ETag version in a variable
+        etagVersion = lineVersionsFirst.etagVersion;
+        expect(etagVersion).to.exist.and.be.a('number');
+      }
+    );
   });
 
   it('Step-5: Update the line version', () => {
     const todayInIso = ReleaseApiUtils.today().toISOString();
     CommonUtils.put(`/line-directory/v2/lines/versions/${lineVersionId}`, {
       swissLineNumber: todayInIso, // Changed, see above
-      lineType: "ORDERLY",
+      lineType: 'ORDERLY',
       businessOrganisation: sboid,
       validFrom: VALID_FROM,
       validTo: ReleaseApiUtils.tomorrowAsAtlasString(), // Changed, see above
       etagVersion: etagVersion,
-      offerCategory: "SL"
+      offerCategory: 'SL',
+      description: 'This is just a description.',
+      number: '1111111',
     }).then((response) => {
-      const lineVersionsFirst = ReleaseApiUtils.makeCommonChecks(response, slnid, lineVersionId);
-      expect(lineVersionsFirst).to.have.property('swissLineNumber').that.equals(todayInIso);
+      const lineVersionsFirst = ReleaseApiUtils.makeCommonChecks(
+        response,
+        slnid,
+        lineVersionId
+      );
+      expect(lineVersionsFirst)
+        .to.have.property('swissLineNumber')
+        .that.equals(todayInIso);
     });
   });
 });
