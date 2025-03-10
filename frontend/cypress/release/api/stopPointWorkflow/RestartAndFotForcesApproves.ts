e1d8c4c83c93e4adbcfc5b9248e1adc437e79e84
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-10 17:06:25 +0100

    joho: ATLAS-2566: Streamline code

--- frontend/cypress/release/api/stopPointWorkflow/RestartAndFotForcesApproves.ts
+++ frontend/cypress/release/api/stopPointWorkflow/RestartAndFotForcesApproves.ts
@@ -1,4 +1,3 @@
-import CommonUtils from '../../../support/util/common-utils';
 import ReleaseApiUtils, {
   RestartStopPointWorkflowData,
   SePoDependentInfo,
@@ -8,66 +7,37 @@ describe(
   'StopPointWorkflow: Scenario Restart and FOT forces approved answers',
   { testIsolation: false },
   () => {
-    let sboid: string;
     let info: SePoDependentInfo;
     let stopPointWorkflowId: number;
     let examinantIds: number[];
     let stopPointWorkflowRestartedId: number;
 
-    const meansOfTransport = ['UNKNOWN'];
-
-    const spatialReference: string = 'LV95';
-    const dateInMonth: string = new Date()
-      .getMonth()
-      .toString()
-      .padStart(2, '0');
-    const north: string = `12051${dateInMonth}`;
-    const east: string = `26520${dateInMonth}`;
-
-    const height: string = '1';
-
     it('Step-1: Login on ATLAS', () => {
       cy.atlasLogin();
     });
 
-    it('Step-2: Create dependent Business Organisation', () => {
-      CommonUtils.createDependentBusinessOrganisation(
-        ReleaseApiUtils.today(),
-        ReleaseApiUtils.today()
-      ).then((sboidOfBO: string) => {
-        sboid = sboidOfBO;
-      });
+    it('Step-2: Create dependent Business Organisation and Service Point', () => {
+      ReleaseApiUtils.createDependentStopPointObjects().then(
+        (sePoDependentInfo: SePoDependentInfo) => {
+          info = sePoDependentInfo;
+        }
+      );
     });
 
-    it('Step-3: Create new Service Point', () => {
-      // The service-point need to have a meansOfTransport,
-      // so that a StopPointWorkflow can be created and started.
-      ReleaseApiUtils.createDependentServicePoint(sboid, meansOfTransport, {
-        servicePointGeolocation: {
-          spatialReference: spatialReference,
-          north: north,
-          east: east,
-          height: height,
-        },
-      }).then((sePoDependentInfo: SePoDependentInfo) => {
-        info = sePoDependentInfo;
-      });
-    });
-
-    it('Step-4: Create the workflow', () => {
+    it('Step-3: Create the workflow', () => {
       ReleaseApiUtils.createStopPointWorkflow(
         info.parentServicePointId,
         info.parentServicePointSloid
       ).then((id: number) => (stopPointWorkflowId = id));
     });
 
-    it('Step-5: Start the workflow', () => {
+    it('Step-4: Start the workflow', () => {
       ReleaseApiUtils.startStopPointWorkflow(stopPointWorkflowId).then(
         (ids: number[]) => (examinantIds = ids)
       );
     });
 
-    it('Step-6: Restart the workflow', () => {
+    it('Step-5: Restart the workflow', () => {
       ReleaseApiUtils.restartStopPointWorkflow(
         stopPointWorkflowId,
         examinantIds
@@ -79,7 +49,7 @@ describe(
       });
     });
 
-    it('Step-7: Override examinants in restarted workflow', () => {
+    it('Step-6: Override examinants in restarted workflow', () => {
       examinantIds.forEach((examinantId) =>
         ReleaseApiUtils.overrideExaminantOfStopPointWorkflow(
           stopPointWorkflowRestartedId,
@@ -90,7 +60,7 @@ describe(
       );
     });
 
-    it('Step-8: Check status of (restarted) workflows', () => {
+    it('Step-7: Check status of (restarted) workflows', () => {
       ReleaseApiUtils.checkWorkflowStatus(stopPointWorkflowId, 'REJECTED');
       ReleaseApiUtils.checkWorkflowStatus(
         stopPointWorkflowRestartedId,
