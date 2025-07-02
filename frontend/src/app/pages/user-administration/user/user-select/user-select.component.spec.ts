2bef9de117f9328551f880c2024d620b9dcb3e50
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-02 13:24:54 +0200

    ATLAS-2060: fix tests

--- frontend/src/app/pages/user-administration/user/user-select/user-select.component.spec.ts
+++ frontend/src/app/pages/user-administration/user/user-select/user-select.component.spec.ts
@@ -61,6 +61,7 @@ describe('UserSelectComponent', () => {
       of([
         {
           sbbUserId: 'u236171',
+          permissions: new Set<Permission>(),
         },
       ])
     );
@@ -86,6 +87,7 @@ describe('UserSelectComponent', () => {
       of([
         {
           sbbUserId: 'u236171',
+          permissions: new Set<Permission>(),
         },
       ])
     );
