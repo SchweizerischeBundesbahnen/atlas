52959802bff919087935f4254893e46a94a02b40
atlas <TechSupport-ATLAS@sbb.ch>
2026-04-10 14:53:55 +0200

    atlas

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1208.0"
+version = "2.1209.0"
 
 dependencies {
     // Spring Boot Starters
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1208.0"
+version = "2.1209.0"
 
 description = "ServicePointDirectory"
 
