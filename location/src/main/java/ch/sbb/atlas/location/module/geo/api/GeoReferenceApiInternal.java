ee0f164376f7efd02c34cb79e259ed6e4940d9ac
atlas <TechSupport-ATLAS@sbb.ch>
2026-04-14 11:32:17 +0200

    ATLAS-2815: finish todos and manual tests

--- location/src/main/java/ch/sbb/atlas/location/module/geo/api/GeoReferenceApiInternal.java
+++ location/src/main/java/ch/sbb/atlas/location/module/geo/api/GeoReferenceApiInternal.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// todo: manual test of update job
 @RequestMapping("/internal/geo-reference")
 public interface GeoReferenceApiInternal {
 
