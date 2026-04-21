9801e14be4011cbeed69a3f48d6ef936e6bc5e2e
atlas <TechSupport-ATLAS@sbb.ch>
2026-04-21 09:17:17 +0200

    [tekton-build-bot] updated version to 2.1230.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1229.0"
+version = "2.1230.0"
 
 description = "Atlas Export Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1229.0"
+version = "2.1230.0"
 
 description = "ServicePointDirectory"
 
