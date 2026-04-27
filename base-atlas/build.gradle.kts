e83f1b24fd739153b75f56211f5a179c52ab8317
atlas <TechSupport-ATLAS@sbb.ch>
2026-04-27 09:45:14 +0200

    [tekton-build-bot] updated version to 2.1239.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1238.0"
+version = "2.1239.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1238.0"
+version = "2.1239.0"
 
 dependencies {
     // Spring Boot Starters
