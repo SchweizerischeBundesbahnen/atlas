d569b3ea81306d414893bd0b905ee9c089b239bd
atlas <TechSupport-ATLAS@sbb.ch>
2026-05-18 11:24:09 +0200

    Make back for tu more congruent

--- frontend/src/app/pages/tth/dossier/detail/bo-dossier-detail/bo-dossier-detail.component.ts
+++ frontend/src/app/pages/tth/dossier/detail/bo-dossier-detail/bo-dossier-detail.component.ts
@@ -3,7 +3,9 @@ import { AtlasButtonComponent } from '../../../../../core/components/button/atla
 import { AtlasLabelFieldComponent } from '@atlas/form';
 import { AtlasSpacerComponent } from '../../../../../core/components/spacer/atlas-spacer.component';
 import { DetailFooterComponent } from '../../../../../core/components/detail-footer/detail-footer.component';
-import { DetailPageContainerComponent } from '../../../../../core/components/detail-page-container/detail-page-container.component';
+import {
+  DetailPageContainerComponent
+} from '../../../../../core/components/detail-page-container/detail-page-container.component';
 import { DetailPageContentComponent } from '../../../../../core/components/detail-page-content/detail-page-content.component';
 import { ScrollToTopDirective } from '../../../../../core/scroll-to-top/scroll-to-top.directive';
 import { StatementSelectComponent } from '../../statement-select/statement-select.component';
@@ -12,11 +14,7 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { DetailFormComponent } from '../../../../../core/leave-guard/leave-dirty-form-guard.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup } from '@angular/forms';
-import {
-  BoAnswerFormGroupBuilder,
-  DossierDetailFormGroup,
-  DossierFormGroupBuilder,
-} from '../dossier-detail-form-group';
+import { BoAnswerFormGroupBuilder, DossierDetailFormGroup, DossierFormGroupBuilder, } from '../dossier-detail-form-group';
 import { TthDossier } from '../../../../../api/model/tthDossier';
 import { DateComponent } from '../../../../../core/form-components/date/date.component';
 import { CommentComponent } from '../../../../../core/form-components/comment/comment.component';
@@ -128,6 +126,7 @@ export class BoDossierDetailComponent implements DetailFormComponent, OnInit {
       this.dossierInternalService.answerQuestion(questionId!, boAnswer).subscribe(() => {
         this.notificationService.success('TTH.DOSSIER.NOTIFICATION.SENT_TO_CANTON');
         this.form.disable();
+        this.form.markAsPristine();
         this.isDossierStatusBoCheck = false;
       });
     }
