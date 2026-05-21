14b29869cdbc12d7582bce5a05081f19266f80db
atlas <TechSupport-ATLAS@sbb.ch>
2026-05-21 11:35:35 +0200

    [tekton-build-bot] updated version to 2.1290.0

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1289.0"
+version = "2.1290.0"
 
 dependencies {
     // Spring Boot Starters
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1289.0"
+version = "2.1290.0"
 
 description = "ServicePointDirectory"
 
