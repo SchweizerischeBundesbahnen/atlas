aa01470d3d5c7dc3176dccc0236b574751bf380e
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-05 09:15:30 +0200

    [tekton-build-bot] updated version to 2.1309.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1308.0"
+version = "2.1309.0"
 
 subprojects {
     sonar {
--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1308.0"
+version = "2.1309.0"
 
 dependencies {
     implementation(project(":base-atlas"))
