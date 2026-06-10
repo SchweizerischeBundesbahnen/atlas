37ac45d8f3decf8a10370a52ee20d233f07d2c0c
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-10 09:46:15 +0200

    ATLAS-3180: extract and test worst accessibility for stoppoint

--- wheelchair-accessibility/src/test/java/ch/sbb/atlas/wheelchairaccessibility/calculator/StopPointWheelchairAccessibilityCalculatorTest.java
+++ wheelchair-accessibility/src/test/java/ch/sbb/atlas/wheelchairaccessibility/calculator/StopPointWheelchairAccessibilityCalculatorTest.java
@@ -5,11 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ch.sbb.atlas.api.prm.enumeration.BooleanOptionalAttributeType;
 import ch.sbb.atlas.api.prm.enumeration.VehicleAccessAttributeType;
 import ch.sbb.atlas.api.prm.model.wheelchairaccessibility.WheelchairAccessibilityState;
+import ch.sbb.atlas.wheelchairaccessibility.calculator.StopPointWheelchairAccessibilityCalculator.WorstAccessibilityCalculator;
 import ch.sbb.atlas.wheelchairaccessibility.model.AccessibilityPlatformTestData;
 import ch.sbb.atlas.wheelchairaccessibility.model.AccessibilityRequest;
 import ch.sbb.atlas.wheelchairaccessibility.model.AccessibilityStopPointTestData;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StopPointWheelchairAccessibilityCalculatorTest {
 
@@ -44,6 +48,38 @@ class StopPointWheelchairAccessibilityCalculatorTest {
             .build());
 
     assertThat(result).isEqualTo(WheelchairAccessibilityState.SHUTTLE);
+  }
+
+  @ParameterizedTest(name = "platform1={0}, platform2={1} -> {2}")
+  @CsvSource({
+      // Defined by Rank
+      "AUTONOMY,            RAMP_USE,          RAMP_USE",
+      "RAMP_USE,            PRE_REGISTRATION,  PRE_REGISTRATION",
+      "PRE_REGISTRATION,    NO_ACCESS,         NO_ACCESS",
+      "NO_INFO,             SHUTTLE,           SHUTTLE",
+
+      // NO_INFO
+      "NO_ACCESS,           NO_INFO,           NO_INFO",
+      "AUTONOMY,            NO_INFO,           NO_INFO",
+  })
+  void shouldCalculateWorstAccessibility(WheelchairAccessibilityState platform1, WheelchairAccessibilityState platform2,
+      WheelchairAccessibilityState expected) {
+    WheelchairAccessibilityState result = new WorstAccessibilityCalculator(List.of(platform1, platform2)).calculate();
+
+    assertThat(result).isEqualTo(expected);
+  }
+
+  @Test
+  void shouldCalculateWorstAccessibilityWithOnePlatform() {
+    WheelchairAccessibilityState result = new WorstAccessibilityCalculator(
+        List.of(WheelchairAccessibilityState.AUTONOMY)).calculate();
+    assertThat(result).isEqualTo(WheelchairAccessibilityState.AUTONOMY);
+  }
+
+  @Test
+  void shouldCalculateWorstAccessibilityWithoutPlatform() {
+    WheelchairAccessibilityState result = new WorstAccessibilityCalculator(Collections.emptyList()).calculate();
+    assertThat(result).isEqualTo(WheelchairAccessibilityState.NO_INFO);
   }
 
 }
