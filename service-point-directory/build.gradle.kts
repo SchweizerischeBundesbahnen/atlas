f72718128c6fd6741307335ccbdd056fc1a42651
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-11 11:22:22 +0200

    updated version to 2.1343.0

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1342.0"
+version = "2.1343.0"
 
 description = "ServicePointDirectory"
 
--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1342.0"
+version = "2.1343.0"
 
 dependencies {
     implementation(project(":base-atlas"))
