2b4d47170a7d8dad2915d475f3bad332879290ec
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-11 15:00:41 +0200

    updated version to 2.1352.0

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1351.0"
+version = "2.1352.0"
 
 description = "ServicePointDirectory"
 
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1351.0"
+version = "2.1352.0"
 
 description = "Atlas Workflow Service"
 
