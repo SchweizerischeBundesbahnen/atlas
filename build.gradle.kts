2b1230d9e213712184e294a5e736c8de93f5ebbe
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-11 11:17:49 +0200

    updated version to 2.1330.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1329.0"
+version = "2.1330.0"
 
 subprojects {
     sonar {
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1329.0"
+version = "2.1330.0"
 
 description = "Atlas Scheduling Service"
 
