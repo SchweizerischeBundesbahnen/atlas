b29239e3544a9992c0afa01a6c06f2b98333ceb5
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-11 11:18:10 +0200

    updated version to 2.1331.0

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1330.0"
+version = "2.1331.0"
 
 dependencies {
     // Spring Boot Starters
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1330.0"
+version = "2.1331.0"
 
 description = "Atlas Scheduling Service"
 
