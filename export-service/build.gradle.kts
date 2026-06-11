029e3185abb61dfcdeb8f5d3bcf39a5c32de6e82
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-11 11:19:17 +0200

    updated version to 2.1334.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1333.0"
+version = "2.1334.0"
 
 description = "Atlas Export Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1333.0"
+version = "2.1334.0"
 
 description = "ServicePointDirectory"
 
