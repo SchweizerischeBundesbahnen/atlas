82b1cbcdc9155f5232f58b7d67ff747329fb0f4d
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-25 14:42:39 +0200

    [tekton-build-bot] updated version to 2.1379.0

--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1378.0"
+version = "2.1379.0"
 
 dependencies {
     implementation(project(":base-atlas"))
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1378.0"
+version = "2.1379.0"
 
 description = "Atlas Workflow Service"
 
