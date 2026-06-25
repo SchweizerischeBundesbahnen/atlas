105eeab1d3a46df730076242c53439750186f39c
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-25 11:33:08 +0200

    [tekton-build-bot] updated version to 2.1378.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1377.0"
+version = "2.1378.0"
 
 description = "Atlas Bulk Import Service"
 
--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1377.0"
+version = "2.1378.0"
 
 dependencies {
     // Spring Boot Starters
