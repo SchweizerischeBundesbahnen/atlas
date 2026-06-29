3748e683a071e782b37d330e87bc0ea61709eef4
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-29 09:10:23 +0200

    [tekton-build-bot] updated version to 2.1382.0

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1381.0"
+version = "2.1382.0"
 
 dependencies {
     // Spring Boot Starters
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1381.0"
+version = "2.1382.0"
 
 description = "ServicePointDirectory"
 
