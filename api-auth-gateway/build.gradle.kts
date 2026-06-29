c7c4cfeaacdb8c04adea678219b8ae379dc15b88
atlas <TechSupport-ATLAS@sbb.ch>
2026-06-29 09:38:53 +0200

    [tekton-build-bot] updated version to 2.1383.0

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1382.0"
+version = "2.1383.0"
 
 dependencies {
     // Spring Boot Starters
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1382.0"
+version = "2.1383.0"
 
 subprojects {
     sonar {
