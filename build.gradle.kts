3eccd22cf97dd72d46004b40f31edeab6aeeb49d
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-01 13:15:33 +0200

    [tekton-build-bot] updated version to 2.1388.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1387.0"
+version = "2.1388.0"
 
 subprojects {
     sonar {
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1387.0"
+version = "2.1388.0"
 
 description = "Atlas Scheduling Service"
 
