2ebf32be30bad78b6b4a44c015b9efe9625b4683
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-09 11:31:26 +0200

    ATLAS-3369: tested setDesignationOperational

--- base-atlas/src/test/java/ch/sbb/atlas/api/servicepoint/CreateTrafficPointElementVersionModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/api/servicepoint/CreateTrafficPointElementVersionModelTest.java
@@ -57,6 +57,33 @@ class CreateTrafficPointElementVersionModelTest extends BaseValidatorTest {
     assertThat(result).hasSize(1);
   }
 
+  @Test
+  void shouldNormalizeNullDesignationOperationalToNull() {
+    CreateTrafficPointElementVersionModel model = getCreateTrafficPointVersionModel();
+
+    model.setDesignationOperational(null);
+
+    assertThat(model.getDesignationOperational()).isNull();
+  }
+
+  @Test
+  void shouldNormalizeBlankDesignationOperationalToNull() {
+    CreateTrafficPointElementVersionModel model = getCreateTrafficPointVersionModel();
+
+    model.setDesignationOperational("   \t");
+
+    assertThat(model.getDesignationOperational()).isNull();
+  }
+
+  @Test
+  void shouldKeepNonBlankDesignationOperationalAsIs() {
+    CreateTrafficPointElementVersionModel model = getCreateTrafficPointVersionModel();
+
+    model.setDesignationOperational("042");
+
+    assertThat(model.getDesignationOperational()).isEqualTo("042");
+  }
+
   private static CreateTrafficPointElementVersionModel getCreateTrafficPointVersionModel() {
     GeolocationBaseCreateModel trafficPointElementGeolocation = GeolocationBaseCreateModel
         .builder()
