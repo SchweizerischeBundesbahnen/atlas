49422942d1ad4ca84e153aa45ae579cc8b968311
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-13 10:18:37 +0200

    [tekton-build-bot] updated version to 2.1404.0

--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1403.0"
+version = "2.1404.0"
 
 description = "Atlas Scheduling Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1403.0"
+version = "2.1404.0"
 
 description= "Atlas User Administration Security Handler"
 
