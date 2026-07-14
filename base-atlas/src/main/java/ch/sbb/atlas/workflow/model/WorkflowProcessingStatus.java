1f69ae57a0c2c5c488a86f2e9c4122f97e369ec6
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-14 14:15:39 +0200

    ATLAS-3475: cleanup base-atlas

--- base-atlas/src/main/java/ch/sbb/atlas/workflow/model/WorkflowProcessingStatus.java
+++ base-atlas/src/main/java/ch/sbb/atlas/workflow/model/WorkflowProcessingStatus.java
@@ -8,10 +8,4 @@ public enum WorkflowProcessingStatus {
   EVALUATED,
   IN_PROGRESS;
 
-  public static WorkflowProcessingStatus getProcessingStatus(WorkflowStatus workflowStatus) {
-    if (WorkflowStatus.APPROVED == workflowStatus || WorkflowStatus.REJECTED == workflowStatus) {
-      return EVALUATED;
-    }
-    return IN_PROGRESS;
-  }
 }
