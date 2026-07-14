3f88d5f5cafbbe6d0d68003c3b00cbdfc0426a35
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-14 09:18:23 +0200

    [tekton-build-bot] updated version to 2.1405.0

--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1404.0"
+version = "2.1405.0"
 
 description = "Atlas User Administration"
 
--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1404.0"
+version = "2.1405.0"
 
 dependencies {
     implementation(project(":base-atlas"))
