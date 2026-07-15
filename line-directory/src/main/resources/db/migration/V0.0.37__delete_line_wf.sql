73d042941b2d40e999f4043efb4d53af3581a8a7
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-15 08:30:21 +0200

    ATLAS-3475: set line status to validated

--- line-directory/src/main/resources/db/migration/V0.0.37__delete_line_wf.sql
+++ line-directory/src/main/resources/db/migration/V0.0.37__delete_line_wf.sql
@@ -3,3 +3,7 @@ drop table line_version_snapshot;
 
 drop sequence line_version_workflow_seq;
 drop sequence line_version_snapshot_seq;
+
+update line_version
+set status = 'VALIDATED'
+where status in ('IN_REVIEW', 'DRAFT');
