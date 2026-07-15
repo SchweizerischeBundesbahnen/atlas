e48d3c8ac39476578afd704730544bcc5c567546
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-15 16:45:55 +0200

    ATLAS-3494: transform canton to shorthand and translate outside the pipe

--- frontend/src/app/core/cantons/display-canton.pipe.ts
+++ frontend/src/app/core/cantons/display-canton.pipe.ts
@@ -1,17 +1,13 @@
-import { inject, Pipe, PipeTransform } from '@angular/core';
-import { SwissCanton } from '../../api';
-import { Cantons } from './Cantons';
-import { TranslatePipe } from '@ngx-translate/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {SwissCanton} from '../../api';
+import {Cantons} from './Cantons';
 
-@Pipe({ name: 'displayCanton' })
-export class DisplayCantonPipe implements PipeTransform {
-  private readonly translatePipe = inject(TranslatePipe);
-
+@Pipe({ name: 'transformCantonToShorthand' })
+export class TransformCantonToShorthandPipe implements PipeTransform {
   transform(value?: SwissCanton): string {
     if (!value) {
       return '-';
     }
-    const translationPath = 'TTH.CANTON.' + Cantons.fromSwissCanton(value)!.short;
-    return this.translatePipe.transform(translationPath);
+    return 'TTH.CANTON.' + Cantons.fromSwissCanton(value)!.short;
   }
 }
