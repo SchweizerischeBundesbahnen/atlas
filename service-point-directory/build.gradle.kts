d2e0c0c1f2f0c4d33bbf4cedb968a4d727b73ca6
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-21 09:12:52 +0200

    [tekton-build-bot] updated version to 2.1427.0

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1426.0"
+version = "2.1427.0"
 
 description = "ServicePointDirectory"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1426.0"
+version = "2.1427.0"
 
 description = "Atlas User Administration"
 
