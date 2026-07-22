56f9b947d22121ced15398978db15f1d71563629
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-22 15:57:14 +0200

    [tekton-build-bot] updated version to 2.1431.0

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1430.0"
+version = "2.1431.0"
 
 dependencies {
     // Spring Boot Starters
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1430.0"
+version = "2.1431.0"
 
 description = "Atlas User Administration"
 
