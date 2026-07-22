0498e70c0116fcb9b7fb3d39eb3170d860987efe
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-22 10:28:58 +0200

    [tekton-build-bot] updated version to 2.1429.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1428.0"
+version = "2.1429.0"
 
 dependencies {
     // Spring Boot Starters
--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1428.0"
+version = "2.1429.0"
 
 description = "Atlas Mail Service"
 
