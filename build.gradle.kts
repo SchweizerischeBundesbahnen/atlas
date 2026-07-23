e026cfd29109b326a035556b3142d21b84473feb
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-23 16:12:28 +0200

    [tekton-build-bot] updated version to 2.1436.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1435.0"
+version = "2.1436.0"
 
 subprojects {
     sonar {
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1435.0"
+version = "2.1436.0"
 
 description = "Atlas User Administration"
 
