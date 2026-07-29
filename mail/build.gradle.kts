8f94e9e07a99e7e304e9e22b4f5cafc690549a6d
atlas <TechSupport-ATLAS@sbb.ch>
2026-07-29 14:48:24 +0200

    atlas

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1452.0"
+version = "2.1453.0"
 
 description = "Atlas Mail Service"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1452.0"
+version = "2.1453.0"
 
 description = "Atlas User Administration"
 
