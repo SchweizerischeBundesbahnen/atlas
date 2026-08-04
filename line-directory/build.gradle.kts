76d95cf611e4e951e7003777f835bfa6a5b10f0e
atlas <TechSupport-ATLAS@sbb.ch>
2026-08-04 09:32:57 +0200

    [tekton-build-bot] updated version to 2.1462.0

--- line-directory/build.gradle.kts
+++ line-directory/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1461.0"
+version = "2.1462.0"
 
 dependencies {
     // Spring Boot Starters
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1461.0"
+version = "2.1462.0"
 
 description = "Atlas Scheduling Service"
 
