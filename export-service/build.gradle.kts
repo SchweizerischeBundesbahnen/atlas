aea6e5c761566e912451cafab8dc5af89bf98395
atlas <TechSupport-ATLAS@sbb.ch>
2026-08-07 09:33:49 +0200

    [tekton-build-bot] updated version to 2.1470.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1469.0"
+version = "2.1470.0"
 
 description = "Atlas Export Service"
 
--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1469.0"
+version = "2.1470.0"
 
 dependencies {
     // Spring Boot Starters
