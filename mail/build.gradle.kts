ba89079f4faf6f4f6f65a702dae88f749ef1b476
atlas <TechSupport-ATLAS@sbb.ch>
2026-08-10 09:31:07 +0200

    [tekton-build-bot] updated version to 2.1475.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1474.0"
+version = "2.1475.0"
 
 description = "Atlas Mail Service"
 
--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1474.0"
+version = "2.1475.0"
 
 dependencies {
     implementation(project(":base-atlas"))
