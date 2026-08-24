ad90be432412f47b20e11b66316a2d8cb3559fdf
atlas <TechSupport-ATLAS@sbb.ch>
2026-08-24 09:03:52 +0200

    [tekton-build-bot] updated version to 2.1513.0

--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1512.0"
+version = "2.1513.0"
 
 description = "Atlas Scheduling Service"
 
--- wheelchair-accessibility/build.gradle.kts
+++ wheelchair-accessibility/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1512.0"
+version = "2.1513.0"
 
 dependencies {
     implementation(project(":base-atlas"))
