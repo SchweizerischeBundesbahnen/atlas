0107d09b7f99fb4b1c082ec4309fdd08bbef723c
atlas <TechSupport-ATLAS@sbb.ch>
2026-09-09 10:40:15 +0200

    [tekton-build-bot] updated version to 2.1546.0

--- prm-directory/build.gradle.kts
+++ prm-directory/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1545.0"
+version = "2.1546.0"
 
 dependencies {
     // Spring Boot Starters
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1545.0"
+version = "2.1546.0"
 
 description = "ServicePointDirectory"
 
