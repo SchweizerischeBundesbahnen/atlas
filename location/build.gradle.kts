77eea44deb9f74d84246f182c16eb96c6b8063fb
atlas <TechSupport-ATLAS@sbb.ch>
2026-09-14 09:09:52 +0200

    [tekton-build-bot] updated version to 2.1558.0

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1557.0"
+version = "2.1558.0"
 
 dependencies {
     // Spring Boot Starters
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1557.0"
+version = "2.1558.0"
 
 description = "Atlas Workflow Service"
 
