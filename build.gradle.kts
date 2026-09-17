3bbe23f0790b65e0ce8561c0a62502b0938d4309
atlas <TechSupport-ATLAS@sbb.ch>
2026-09-17 15:05:36 +0200

    [tekton-build-bot] updated version to 2.1571.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1570.0"
+version = "2.1571.0"
 
 // A subproject is considered a runnable Spring Boot service (rather than a shared library module)
 // when it applies the "buildlogic.docker-java" convention plugin, recognizable by its "prepareJavaDockerContext" task.
